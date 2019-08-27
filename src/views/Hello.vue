38972b13a666bc75577e09fd5c0b98376d3091e2
GMartigny <guillaume.martigny@gmail.com>
2019-08-27 13:19:30 +0200

    Add vue property decorator

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -4,7 +4,9 @@
 
 <script lang="ts">
     import Vue from 'vue';
+    import { Component } from "vue-property-decorator";
 
+    @Component
     export default class Hello extends Vue {
         message:string = 'Hello';
     }
