d3baa0c6c44a2274e052317ea304f4e68199b966
Gwenael Provost <gwenael.provost@kanoma.fr>
2020-01-26 00:13:54 +0100

    :wrench: Add HMR and vue-router

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,75 @@
-const { resolve } = require('path');
+const { join, resolve } = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.ts',
-    output: {
-        path: resolve(__dirname, './public'),
-    },
-    resolve: {
-        alias: {
-            vue$: 'vue/dist/vue.esm.js',
+
+module.exports = (env, options) => {
+    process.env.NODE_ENV = options.mode;
+    const scssLoader = process.env.NODE_ENV !== 'production' ? 'vue-style-loader' : MiniCssExtractPlugin.loader;
+
+    return {
+        entry: './src/app.ts',
+        output: {
+            path: resolve(__dirname, './public'),
         },
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                extractComments: true,
+        resolve: {
+            alias: {
+                vue$: 'vue/dist/vue.esm.js',
+            },
+        },
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: true,
+                }),
+            ],
+        },
+        module: {
+            rules: [
+                { // Vue
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                },
+                { // TypeScript
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/],
+                    },
+                },
+                { // SASS
+                    test: /\.s?css$/,
+                    use: [
+                        scssLoader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+                { // Others
+                    test: /\.(eot|woff2?|ttf)$/,
+                    use: 'file-loader',
+                },
+            ],
+        },
+        devServer: {
+            contentBase: join(__dirname, 'dist'),
+            hot: true,
+            watchOptions: {
+                poll: true,
+            },
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Leitner box',
+                template: './src/template.html',
+                inject: true,
             }),
         ],
-    },
-    module: {
-        rules: [
-            { // Vue
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
-            { // TypeScript
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                options: {
-                    appendTsSuffixTo: [/\.vue$/],
-                },
-            },
-            { // CSS
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
-            { // SASS
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            { // Others
-                test: /\.(eot|woff2?|ttf)$/,
-                use: 'file-loader',
-            },
-        ],
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Leitner box',
-            template: './src/template.html',
-        }),
-    ],
+    };
 };
