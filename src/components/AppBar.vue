4889c7ce1437e218a87e0d3f082fa41e5cb9d3b4
Guillaume Martigny <guillaume.martigny@gmail.com>
2020-02-12 19:43:26 +0100

    Add menu button

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,5 +1,9 @@
 <template>
     <v-app-bar app>
+        <v-btn icon>
+            <v-icon>mdi-menu</v-icon>
+        </v-btn>
+
         <v-toolbar-title>
             Leitner
         </v-toolbar-title>
