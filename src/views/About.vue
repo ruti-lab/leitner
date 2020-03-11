89e7b1cf277b42d4409313e97b2f4a8b21eec8f4
Guillaume Martigny <guillaume.martigny@gmail.com>
2020-03-11 11:36:08 +0100

    :sparkles: First draft for the about vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,29 @@
 <template>
-    <v-card>
-        <v-list
-            subheader
-            two-line
-        >
-            <v-subheader>App info</v-subheader>
-
-            <v-list-item>
-                <v-list-item-content>
-                    <v-list-item-title>Open Source Project</v-list-item-title>
-                    <v-list-item-subtitle>Leitner is open source ! Check it out on GitHub</v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
-
-            <v-list-item>
-                <v-list-item-content>
-                    <v-list-item-title>App Version</v-list-item-title>
-                    <v-list-item-subtitle>14.42.666</v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list>
-    </v-card>
+    <v-container>
+        <v-card>
+            <v-card-title>App infos</v-card-title>
+            <v-card-text>
+                <v-list>
+                    <v-list-item
+                        v-for="(item, index) in list"
+                        :key="index"
+                    >
+                        <v-list-item-content>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                <v-list-item-subtitle v-html="item.content" />
+                            </v-list-item-content>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { version } from '../../package.json';
 
 @Component
 /**
@@ -32,6 +31,30 @@ import { Vue, Component } from 'vue-property-decorator';
  * @extends Vue
  */
 export default class About extends Vue {
+    list = [
+        {
+            title: 'Open Source Project',
+            content: 'Leitner is open source ! Check it out on <a href="https://github.com/ruti-lab/leitner">Github</a>.',
+        },
+        {
+            title: 'Issues',
+            content: 'You encounter a bug or have trouble with the app ? '
+                + 'Send us <a href="https://github.com/ruti-lab/leitner/issues/new">an issue</a>.',
+        },
+        {
+            title: 'Version',
+            content: `v${version}`,
+        },
+        {
+            title: 'Privacy policy',
+            content: 'We take your privacy very seriously and will never sell your data away. '
+                + 'Read our <a href="">privacy policy</a> for more information.',
+        },
+        {
+            title: 'RUTI-lab',
+            content: '©️ 2020',
+        },
+    ];
 }
 </script>
 
